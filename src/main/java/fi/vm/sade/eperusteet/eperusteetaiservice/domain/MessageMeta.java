3c21fbbf2d562cb2fb6f832390936f5516e61530
Olli-Pekka Lehtokallio <olli-pekka.lehtokallio@gofore.com>
2024-09-30 14:52:24 +0300

    meta changes

--- src/main/java/fi/vm/sade/eperusteet/eperusteetaiservice/domain/MessageMeta.java
+++ src/main/java/fi/vm/sade/eperusteet/eperusteetaiservice/domain/MessageMeta.java
@@ -1,10 +1,13 @@
 package fi.vm.sade.eperusteet.eperusteetaiservice.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 import java.util.Map;
 
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class MessageMeta {
     private String educationLevel;
     private Map<String, String> sourceName;
@@ -12,4 +15,9 @@ public class MessageMeta {
     private Integer sourceId;
     private String sourceLanguage;
     private Integer sourceRevision;
+    private String instructions;
+    private Double temperature;
+    @JsonProperty("top_p")
+    private Double topP;
+    private String model;
 }

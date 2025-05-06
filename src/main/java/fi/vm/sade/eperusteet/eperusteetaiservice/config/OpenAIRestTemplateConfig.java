8c349bfb9a588604b5b546d0a8d96804fc7b8258
Olli-Pekka Lehtokallio <olli-pekka.lehtokallio@gofore.com>
2025-05-06 15:58:24 +0300

    restclient-timeout

--- src/main/java/fi/vm/sade/eperusteet/eperusteetaiservice/config/OpenAIRestTemplateConfig.java
+++ src/main/java/fi/vm/sade/eperusteet/eperusteetaiservice/config/OpenAIRestTemplateConfig.java
@@ -3,6 +3,8 @@ package fi.vm.sade.eperusteet.eperusteetaiservice.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 
 @Configuration
@@ -17,7 +19,15 @@ public class OpenAIRestTemplateConfig {
         return RestClient.builder()
                 .defaultHeader("Authorization", "Bearer " + openaiApiKey)
                 .defaultHeader("OpenAI-Beta", "assistants=v2")
+                .requestFactory(getClientHttpRequestFactory())
                 .build();
+    }
+
+    private ClientHttpRequestFactory getClientHttpRequestFactory() {
+        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+        factory.setReadTimeout(30_000);
+        factory.setConnectTimeout(30_000);
+        return factory;
     }
 
 }
